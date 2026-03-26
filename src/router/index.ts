010d0d258ee5a12b522265bcc74d7b830464e9ee
残影 <888454@gmail.com>
2026-03-26 18:05:15 +0800

    更新 index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,29 @@ const router = createRouter({
       name: 'home',
       component: () => import('../views/HomeView.vue'),
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('../views/AdminView.vue'),
+    },
   ],
+})
+
+// 管理页面路由守卫
+router.beforeEach((to, from, next) => {
+  const token = sessionStorage.getItem('admin_token')
+  if (to.name === 'admin' && !token) {
+    next('/login')
+  } else if (to.name === 'login' && token) {
+    next('/admin')
+  } else {
+    next()
+  }
 })
 
 export default router
